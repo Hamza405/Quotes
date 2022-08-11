be67e89547d50a07a1bb2a4065de423ead1217de
Hamza <hamzash863@gmail.com>
2022-08-11 09:46:22 -0400

    add propmt to quote form

--- src/pages/NewQuotes.js
+++ src/pages/NewQuotes.js
@@ -1,8 +1,15 @@
 import QuoteForm from '../components/quotes/QuoteForm';
+import { useHistory } from "react-router-dom";
+
 const NewQuote = () => {
+    const history = useHistory();
+
     const addQuoteHandler = ( data ) => {
         console.log( data );
+
+        history.replace( '/quotes' );
     };
+
     return <QuoteForm onAddQuote={ addQuoteHandler } />;
 };
 
