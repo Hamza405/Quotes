ee5dffaa75ddce3ed78bcc4400a7a26f89e07f2f
Hamza <hamzash863@gmail.com>
2022-08-12 13:51:04 -0400

    fetch single quote by id

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,27 +1,41 @@
 import { Route, useParams, Link, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
-
-const data = [
-    { id: '1', author: 'Hamza', text: 'Leargning programing' },
-    { id: '2', author: 'Ali', text: 'Leargning React bebe' }
-];
-
+import { getSingleQuote } from "../services/api";
+import useHttp from "../hooks/use-http";
+import { useEffect } from "react";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 const QuoteDetails = () => {
     const match = useRouteMatch();
     const params = useParams();
+    const { sendRequest, status, data: loadedData, error } = useHttp( getSingleQuote, true );
 
-    const quote = data.find( q => q.id === params.quoteId );
-    if ( !quote )
+    useEffect( () => {
+        sendRequest( params.quoteId );
+    }, [ sendRequest, params.quoteId ] );
+
+    if ( status === 'pending' )
+    {
+        return <div className="centered">
+            <LoadingSpinner />
+        </div>;
+    }
+
+    if ( error )
+    {
+        return <p className="centered">{ error }</p>;
+    }
+
+    if ( !loadedData.text )
     {
         return <p>No Quote Found</p>;
     }
 
     return <div>
         <HighlightedQuote
-            author={ quote.author }
-            text={ quote.text } />
+            author={ loadedData.author }
+            text={ loadedData.text } />
         <Route path={ match.url } exact>
             <div className="centered"><Link className="btn--flat" to={ `${ match.url }/comments` }>Load comments</Link></div>
         </Route>
