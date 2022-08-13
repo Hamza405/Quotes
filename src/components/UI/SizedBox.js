d12d4011581bde4d9740d93f9412b6dcb3d8c836
Hamza <hamzash863@gmail.com>
2022-08-13 13:52:59 -0400

    Init Auth Feature

---
+++ src/components/UI/SizedBox.js
@@ -0,0 +1,6 @@
+const SizedBox = ( props ) => {
+    const size = props.size != null ? props.size : 0.8;
+    return <div style={ { margin: `${ size }rem` } }></div>;
+};
+
+export default SizedBox;
---
+++ src/services/auth-api.js
@@ -0,0 +1,8 @@
+// sign up
+// https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+
+// login
+// https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+
+//forget password
+// https://identitytoolkit.googleapis.com/v1/accounts:update?key=[API_KEY]
