203af8b7c62ce266e3da5f8150bd92561af9d035
Hamza <hamzash863@gmail.com>
2022-08-13 05:54:49 -0400

    Implement lazy loading to second pages

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
+import React, { Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import NewQuote from './pages/NewQuotes';
-import QuoteDetails from './pages/QuoteDetails';
 import Quotes from './pages/Quotes';
 import Layout from './components/layout/Layout';
-import NotFound from './pages/NotFound';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const QuoteDetails = React.lazy( () => import( './pages/QuoteDetails' ) );
+const NewQuote = React.lazy( () => import( './pages/NewQuotes' ) );
+const NotFound = React.lazy( () => import( './pages/NotFound' ) );
 
 
 function App () {
   return <Layout>
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes" />
-      </Route>
-      <Route path='/quotes' exact>
-        <Quotes />
-      </Route>
-      <Route path='/quotes/:quoteId' >
-        <QuoteDetails />
-      </Route>
-      <Route path='/new-quote'>
-        <NewQuote />
-      </Route>
-      <Route path='*'>
-        <NotFound />
-      </Route>
-    </Switch>
+    <Suspense fallback={ <div className='centered'><LoadingSpinner /></div> }>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path='/quotes' exact>
+          <Quotes />
+        </Route>
+        <Route path='/quotes/:quoteId' >
+          <QuoteDetails />
+        </Route>
+        <Route path='/new-quote'>
+          <NewQuote />
+        </Route>
+        <Route path='*'>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Suspense>
   </Layout>;
 }
 
